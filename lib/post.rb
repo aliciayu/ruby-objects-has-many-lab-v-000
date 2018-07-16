cac5cbdcad5b0f5e2e5ab3624a6f85c46880cedc
Alicia Yu <alicia.yu.16@gmail.com>
2018-07-16 22:21:56 +0000

    automatically backed up by learn

---
+++ lib/post.rb
@@ -0,0 +1,22 @@
+class Post
+  attr_accessor :name, :author
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def title
+    title = name
+  end
+
+  def author_name
+    if @author = nil
+      post = nil
+    elsif
+      @author = author
+    end
+  end
+
+end
